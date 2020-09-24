6abe2313536335150e01eecfaa5ac9100c0ed8b7
Christian Dirnhofer <christian@dirnhofer.net>
2020-09-24 19:34:36 +0200

    changed prepare-messages to send-messages

--- src/zigbee2mqtt.js
+++ src/zigbee2mqtt.js
@@ -2,12 +2,52 @@ module.exports = function(RED) {
     const bavaria = require("node-red-ext-bavaria-black");
     
     function genericLamp(config) {
+
         RED.nodes.createNode(this,config);
         var bridgeConfig = RED.nodes.getNode(config.bridge);
         var deviceConfig = RED.nodes.getNode(config.device);
         var node = this;
+        var topic = bridgeConfig.baseTopic + "/" + deviceConfig.deviceName
+        bavaria.observer.register(topic, function(msg){
+            var status = "grey";
+            var text = "Lm: " + msg.brightness;
+            switch(msg.state) {
+                case "ON":
+                case "1":
+                case 1:
+                case "true":
+                case true:
+                    status = "green";
+                    break;
+                case "OFF":
+                case "0":
+                case 0:
+                case "false":
+                case false:
+                    status = "red";
+                    break;
+            }
 
+            if(msg.color_temp !== undefined) {
+                text += " T: " + msg.color_temp;
+            }
+
+            if(msg.color !== undefined)
+            {
+                var rgb = msg.color;
+                if (rgb.x !== undefined) {
+                    rgb = bavaria.converter.xyToRgb(msg.color.x, msg.color.y);
+                }
+
+                text += " RGB: (" + rgb.r + ", " + rgb.g +", " + rgb.b + ")";
+            }
+
+            node.status({fill: status, shape: "dot", text: text});
+        });
+
+        node.status({fill: "gray", shape: "dot", text: "pending"});
         node.on('input', function(msg) {
+            
             if(msg.payload.devices === undefined)
             {
                 msg.payload.devices = [];
@@ -41,6 +81,7 @@ module.exports = function(RED) {
 
             msg.payload.devices.push(device);
 
+
             node.send(msg);
         });
     }
@@ -62,14 +103,49 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         this.name   = config.name;
         this.baseTopic = config.baseTopic;
+        this.broker = config.broker;
+        this.requireLogin = config.requireLogin;
     }
     
-    RED.nodes.registerType("zigbee2mqtt-bridge-config", bridgeConfig)
+    RED.nodes.registerType("zigbee2mqtt-bridge-config", bridgeConfig, {
+        credentials: {
+            username: {type:"text"},
+            password: {type:"password"}
+        }
+    });
     
-    function prepareMessages(config){
+    function sendMessages(config){
         RED.nodes.createNode(this,config);
+        
         var node = this;
+        var bridgeConfig = RED.nodes.getNode(config.bridge);
+        var mqtt = require('mqtt')
+        var options = undefined;
+        if (bridgeConfig.requireLogin) {
+            options = {
+                username: bridgeConfig.credentials.username,
+                password: bridgeConfig.credentials.password
+            };
+        }
+        
+        var client  = mqtt.connect(bridgeConfig.broker, options);
+        client.on('connect', function () {
+            client.subscribe(bridgeConfig.baseTopic + "/+", function(err){
+                if(!err) {
+                }
+            });
+        });
 
+        client.on('message', function(topic, message){
+            var message = JSON.parse(message.toString());
+            message.topic = topic;
+            
+            bavaria.observer.notify(topic, message);
+        });
+
+        node.on('close', function(){
+            client.end();
+        })
         node.on('input', function(msg) {
             var totalDelay = 0;
             var messages = [];
@@ -89,7 +165,8 @@ module.exports = function(RED) {
                         && element.temperature !== undefined
                         && element.temperature !== "")
                     {
-                        element.temperature = msg.payload.override.temperature;
+                        element.temperature = undefined;
+                        element.color_temp = msg.payload.override.temperature;
                     }
                     
                     if (msg.payload.override.color !== undefined 
@@ -109,7 +186,14 @@ module.exports = function(RED) {
 
             function sendNextMessage()
             {
-                node.send(messages[i]);
+                var message = {
+                    payload: messages[i],
+                    topic: messages[i].topic,
+                } 
+                    
+                //bavaria.observer.notify(message.topic, message.payload);
+                client.publish(message.topic + "/set", JSON.stringify(message.payload));
+
                 i++;
 
                 if(i<messages.length)
@@ -133,7 +217,7 @@ module.exports = function(RED) {
         });
     }
 
-    RED.nodes.registerType("prepare-messages", prepareMessages);
+    RED.nodes.registerType("send-messages", sendMessages);
     
     function overrideBrightness(config){
         RED.nodes.createNode(this,config);
