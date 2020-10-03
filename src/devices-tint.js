167b367729a98c254185e39dc4b3e54044096de1
Christian Dirnhofer <christian@dirnhofer.net>
2020-10-03 18:10:41 +0200

    changed tint-remote payload and some fixes

--- src/devices-tint.js
+++ src/devices-tint.js
@@ -10,97 +10,104 @@ module.exports = function (RED) {
         utils.setConnectionState(bridgeNode, node);
         bavaria.observer.register(bridgeNode.id + "_connected", function (message) {
             node.status({ fill: "green", text: "connected" });
-            bridgeNode.subscribeDevice(node.id, config.deviceName, function (message) {
-                try {
-                    const ioMap = {
-                        on: utils.createButtonOutput(0, "power", "pressed", "on"),
-                        off: utils.createButtonOutput(0, "power", "pressed", "off"),
-                        color_wheel: utils.createButtonOutput(1, "color", "pressed", message.action_color),
-                        color_temp: utils.createButtonOutput(2, "temperature", "pressed", message.action_color_temperature),
-                        brightness_up_click: utils.createButtonOutput(3, "brightness_up", "pressed"),
-                        brightness_up_hold: utils.createButtonOutput(3, "brightness_up", "hold"),
-                        brightness_up_release: utils.createButtonOutput(3, "brightness_up", "released"),
-                        brightness_down_click: utils.createButtonOutput(4, "brightness_down", "pressed"),
-                        brightness_down_hold: utils.createButtonOutput(4, "brightness_down", "hold"),
-                        brightness_down_release: utils.createButtonOutput(4, "brightness_down", "released"),
-                        scene_3: utils.createButtonOutput(5, "scene", "pressed", 0),
-                        scene_1: utils.createButtonOutput(5, "scene", "pressed", 1),
-                        scene_2: utils.createButtonOutput(5, "scene", "pressed", 2),
-                        scene_6: utils.createButtonOutput(5, "scene", "pressed", 3),
-                        scene_4: utils.createButtonOutput(5, "scene", "pressed", 4),
-                        scene_5: utils.createButtonOutput(5, "scene", "pressed", 5),
-                    };
+        });
 
-                    var output = ioMap[message.action];
-                    var msg = {
-                        remoteControl: {
-                            button_name: output.button_name,
-                            button_type: output.button_type,
-                            button_payload: output.button_payload,
-                            action_group: message.action_group
+        bridgeNode.subscribeDevice(node.id, config.deviceName, function (message) {
+            try {
+                const ioMap = {
+                    on: utils.createButtonOutput(0, "power", "pressed", config.suppressPowerpayload ? "" :"on"),
+                    off: utils.createButtonOutput(0, "power", "pressed", config.suppressPowerpayload ? "" :"off"),
+                    color_wheel: utils.createButtonOutput(1, "color", "pressed",{
+                        rgb: bavaria.converter.xyToRgb(message.action_color.x, message.action_color.y),
+                        xy: message.action_color
+                    } ),
+                    color_temp: utils.createButtonOutput(2, "temperature", "pressed", message.action_color_temperature),
+                    brightness_up_click: utils.createButtonOutput(3, "brightness_up", "pressed"),
+                    brightness_up_hold: utils.createButtonOutput(3, "brightness_up", "hold"),
+                    brightness_up_release: utils.createButtonOutput(3, "brightness_up", "released"),
+                    brightness_down_click: utils.createButtonOutput(4, "brightness_down", "pressed"),
+                    brightness_down_hold: utils.createButtonOutput(4, "brightness_down", "hold"),
+                    brightness_down_release: utils.createButtonOutput(4, "brightness_down", "released"),
+                    scene_3: utils.createButtonOutput(5, "scene", "pressed", 0),
+                    scene_1: utils.createButtonOutput(5, "scene", "pressed", 1),
+                    scene_2: utils.createButtonOutput(5, "scene", "pressed", 2),
+                    scene_6: utils.createButtonOutput(5, "scene", "pressed", 3),
+                    scene_4: utils.createButtonOutput(5, "scene", "pressed", 4),
+                    scene_5: utils.createButtonOutput(5, "scene", "pressed", 5),
+                };
+
+                var output = ioMap[message.action];
+                var msg = {
+                    remoteControl: {
+                        button_name: output.button_name,
+                        button_type: output.button_type,
+                        button_payload: output.button_payload,
+                        action_group: message.action_group
+                    }
+                };
+
+                switch (output.button_name) {
+                    case "scene":
+                        msg.command = "set";
+                        msg.scene = output.button_payload;
+                        break;
+                    case "color":
+                        msg.payload = {
+                            override: {
+                                color: output.button_payload.rgb
+                            }
                         }
-                    };
-
-                    switch (output.button_name) {
-                        case "scene":
-                            msg.command = "set";
-                            msg.scene = output.button_payload;
-                            break;
-                        case "color":
-                            msg.payload = {
-                                override: {
-                                    color: output.button_payload
+                        break;
+                    case "temperature":
+                        msg.payload = {
+                            override: {
+                                action: {
+                                    name: "color_temp_step",
+                                    value: 50
                                 }
                             }
-                            break;
-                        case "temperature":
+                        }
+                        break;
+                    case "brightness_up":
+                        if (output.button_type == "pressed") {
                             msg.payload = {
                                 override: {
                                     action: {
-                                        name: "color_temp_step",
+                                        name: "brightness_step",
                                         value: 50
                                     }
                                 }
                             }
-                            break;
-                        case "brightness_up":
-                            if (output.button_type == "pressed") {
-                                msg.payload = {
-                                    override: {
-                                        action: {
-                                            name: "brightness_step",
-                                            value: 50
-                                        }
+                        }
+                        break;
+                    case "brightness_down":
+                        if (output.button_type == "pressed") {
+                            msg.payload = {
+                                override: {
+                                    action: {
+                                        name: "brightness_step",
+                                        value: -50
                                     }
                                 }
                             }
-                            break;
-                        case "brightness_down":
-                            if (output.button_type == "pressed") {
-                                msg.payload = {
-                                    override: {
-                                        action: {
-                                            name: "brightness_step",
-                                            value: -50
-                                        }
-                                    }
-                                }
-                            }
-                            break;
-                    }
-
-                    utils.sendAt(node, output.index, msg);
-                    node.status({ fill: "green", "text": "Last action: " + output.button_type });
-                    setTimeout(function () {
-                        node.status({ fill: "green", text: "connected" });
-                    }, 2000);
-                } catch (err) {
-                    node.error(err);
-                    node.status({ fill: "red", "text": "error" });
+                        }
+                        break;
                 }
-            });
+
+                utils.sendAt(node, output.index, msg);
+                node.status({ fill: "green", "text": "Last action: " + output.button_type });
+                setTimeout(function () {
+                    utils.setConnectionState(bridgeNode, node);
+                }, 2000);
+            } catch (err) {
+                node.error(err);
+                node.status({ fill: "red", "text": "error" });
+            }
         });
 
+        node.on('close', ()=>{
+            bridgeNode.unsubscribe(node.id);
+        });
     }
     RED.nodes.registerType("tint-remote", tintRemote);
 }
