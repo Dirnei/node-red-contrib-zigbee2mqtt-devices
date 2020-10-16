daf0928e87fdad0ab697d70db9007681f0bcc7f1
Christian Dirnhofer <christian@dirnhofer.net>
2020-10-16 21:18:00 +0200

    fixed getDeviceList

--- src/zigbee2mqtt-config.js
+++ src/zigbee2mqtt-config.js
@@ -19,8 +19,13 @@ module.exports = function (RED) {
 
     function bridgeConfig(config) {
         RED.nodes.createNode(this, config);
+        
+        const EventEmitter = require("events");
+        const emitter = new EventEmitter();
+
         var node = this;
         var mqttNode = RED.nodes.getNode(config.mqtt);
+        var globalContext = node.context().global;
 
         this.name = config.name;
         this.baseTopic = config.baseTopic;
@@ -28,6 +33,16 @@ module.exports = function (RED) {
         this.isConnected = mqttNode.isConnected;
         this.isReconnecting = mqttNode.reconnecting;
         this.publish = mqttNode.publish;
+        this.knownDevices = globalContext.get("knownDevices" + node.id.replace(".", "_")) || [];
+
+        this.on = function(event, listener){
+            emitter.on(event, listener);
+        };
+
+        this.getDeviceList = function () {
+            return this.knownDevices;
+        };
+
         this.subscribeDevice = function (nodeId, device, callback) {
             var topic = node.baseTopic + "/" + device;
             mqttNode.subscribeDevice(nodeId, topic, callback, true);
@@ -42,10 +57,37 @@ module.exports = function (RED) {
 
         var subId = bavaria.observer.register(mqttNode.id + "_connected", function (_msg) {
             mqttNode.subscribe(node.id, node.baseTopic + "/+", (msg) => { });
+            mqttNode.subscribe(node.id + 1, node.baseTopic + "/bridge/log", (msg) => {
+                switch (msg.type) {
+                    case "devices":
+                        msg.message.forEach(device => {
+                            var d = node.knownDevices.find(e => {
+                                return e.ieeeAddr === device.ieeeAddr;
+                            });
+                            if (d) {
+                                var index = node.knownDevices.indexOf(d);
+                                node.knownDevices.splice(index, 1, device);
+                            }
+
+                            node.knownDevices.push(device);
+                        });
+
+                        globalContext.set("knownDevices_" + node.id.replace(".", "_"), node.knownDevices);
+                        break;
+                    case "groups":
+                        break;
+                }
+
+                emitter.emit("bridge-log", msg);
+            });
+
+            mqttNode.publish(config.baseTopic + "/bridge/config/devices", "{}");
             bavaria.observer.notify(node.id + "_connected");
         });
 
-        node.on("close", function(){
+        node.on("close", function () {
+            mqttNode.unsubscribe(node.id);
+            mqttNode.unsubscribe(node.id + 1);
             bavaria.observer.unregister(subId);
         });
     }
