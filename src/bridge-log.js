73580ea378b9baba76283e5d5597f955426b69e1
Andreas Huber <andreas.huber@infinite-coding.com>
2020-11-21 23:08:07 +0100

    Bridge log working implementation

--- src/bridge-log.js
+++ src/bridge-log.js
@@ -7,22 +7,17 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         const bridgeNode = RED.nodes.getNode(config.bridge);
         const node = this;
-
         const outputHandler = new OutputHandler();
-        
-        outputHandler.addOutput(0, "Consolidated logs", "consolidated_logs", "Consolidated logs from te bridge log", msg => { return msg.message; });
-        
-        let outputCount = 1;
 
         // Create a list of log message types to listen to
         const enabledLogTypes = new Array();
+        let outputCount = 0;
 
         Object.keys(config).forEach(function(key) {
             if(key.startsWith("type_") && config[key]) {
                 const logType = key.replace("type_", "");
                 enabledLogTypes.push(logType);
-                node.warn(logType);
-                outputHandler.addOutput(outputCount, logType, logType, "Logs with the type: " + logType, msg => { return msg.message; });
+                outputHandler.addOutput(outputCount, logType, logType, `Logs with the type: ${logType}`, msg => { return msg.message; });
                 outputCount++;
             }
         });
@@ -33,12 +28,21 @@ module.exports = function (RED) {
             
             // Check if the log type is configured
             if (enabledLogTypes.indexOf(message.type) >= 0) {
-                const leberkas = outputHandler.prepareOutput("type", message);
-                //leberkas.tail.payload = message;
-                node.send();
+                let messageForNOutputs = outputHandler.prepareOutput("type", message);
+
+                if(config.consolidate_output) {
+                    // If all messages shall be consolidated into one output,
+                    // use the last element of the array.
+                    // Because the output handler set's all the other
+                    // indexes to null - so nothing is sent to those outputs.
+                    messageForNOutputs = messageForNOutputs.slice(-1);
+                }
+                
+                node.send(messageForNOutputs);
             }
         });
 
+        // Set the node status
         bavaria.observer.register(bridgeNode.id + "_connected", function (message) {
             node.status({ fill: "green", text: "connected" });
         });
