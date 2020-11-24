3ef5bae7459382cb6803bdd4d7d92a27dd46c8a1
Christian Dirnhofer <christian@dirnhofer.net>
2020-11-24 22:45:31 +0100

    basic functions are working

--- src/devices-tasmota.js
+++ src/devices-tasmota.js
@@ -4,20 +4,23 @@ module.exports = function (RED) {
 
     function createTasmota(config) {
         RED.nodes.createNode(this, config);
+        var deviceConfig = RED.nodes.getNode(config.device);
         var node = this;
         const possibleValues = ["on", "off", "toggle", "0", "1", "2"];
 
+
+        node.warn(config);
+
         function preparePayload(msg)
         {
-            node.warn(msg);
-            return msg;
+            return msg.state;
         }
 
         node.on("input", function (msg) {
             if (possibleValues.includes(msg.payload.toString().toLowerCase()))
             {
                 utils.payloads.devices.addDevice(msg, {
-                    topic: `cmnd/${config.topic}/power`,
+                    topic: deviceConfig.commandTopic + "/POWER",
                     state: utils.payloads.convertToOnOff(msg.payload),
                     target: "mqtt",
                     payloadGenerator: preparePayload
