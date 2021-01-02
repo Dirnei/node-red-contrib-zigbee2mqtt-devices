416a1250923b054b8a28bf6d67260b1ccd33e928
Christian Dirnhofer <christian@dirnhofer.net>
2021-01-02 17:14:20 +0100

    added notification when device list fetching failed

--- src/_api.js
+++ src/_api.js
@@ -1,14 +1,22 @@
+const devicesIkea = require("./nodes/devices-ikea.js");
+
 module.exports = function (RED) {
     const utils = require("./lib/utils.js");
     const bavaria = utils.bavaria();
 
     RED.httpAdmin.get("/z2m/devices/:broker/:deviceType/:vendor/:model", function (req, res) {
         try {
+            var response = {
+                success: false,
+                message: "",
+                devices: []
+            };
+
             var broker = RED.nodes.getNode(req.params.broker.replace("_", "."));
 
-            if(broker === undefined || broker === null)
-            {
-                res.end("{devices: []}");
+            if (broker === undefined || broker === null) {
+                response.message = "Unable to find broker. Pleas deploy first and try it again.";
+                res.end(JSON.stringify(response));
                 return;
             }
 
@@ -23,34 +31,48 @@ module.exports = function (RED) {
                 model = [model];
             }
 
-            res.end(JSON.stringify({
-                devices: devices.filter(e => {
-                    try {
-                        var dt = e.type.toLowerCase();
-                        var dv = "all";
-                        var dm = "all";
+            response.devices = filterDevices(devices, type, vendor, model);
+            response.success = devices.length > 0;
+            if(!response.success)
+            {
+                response.message = "No devices found!";
+            }
 
-                        if (e.vendor) {
-                            dv = e.vendor.toLowerCase();
-                        }
+            console.log("-------------------------------------------");
+            console.log(response);
+            console.log("-------------------------------------------");
 
-                        if (e.model) {
-                            dm = e.model.toLowerCase();
-                        }
-
-                        return (dt == type || (type == "enddevice" && dt == "greenpower") || (type == "all" && dt !== "coordinator")) &&
-                            (dv == vendor || (vendor == "all")) &&
-                            ((model == "all") || model.includes(dm));
-                    } catch (err) {
-                        console.log(err);
-                        console.log(e);
-                    }
-                })
-            }));
+            res.end(JSON.stringify(response));
         } catch (err) {
+            res.end(JSON.stringify(response));
             console.log(err);
         }
     });
+
+    function filterDevices(devices, type, vendor, model){
+        return devices.filter(e => {
+            try {
+                var dt = e.type.toLowerCase();
+                var dv = "all";
+                var dm = "all";
+
+                if (e.vendor) {
+                    dv = e.vendor.toLowerCase();
+                }
+
+                if (e.model) {
+                    dm = e.model.toLowerCase();
+                }
+
+                return (dt == type || (type == "enddevice" && dt == "greenpower") || (type == "all" && dt !== "coordinator")) &&
+                    (dv == vendor || (vendor == "all")) &&
+                    ((model == "all") || model.includes(dm));
+            } catch (err) {
+                console.log(err);
+                console.log(e);
+            }
+        });
+    }
 
     RED.httpAdmin.get("/z2m/scenes", function (req, res) {
         try {
